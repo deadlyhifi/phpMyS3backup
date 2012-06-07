4c051d380eb2fdda1a21a1324414e36dc90d94a5
deadlyhifi <tom@deadlyhifi.com>
2012-06-07 12:01:02 +0100

    Tweaked for me.
    Define a bucket to use for storage.
    Ability to set timezone.
    Change date format to be more Ingerlish. dd-mm-yy.

--- phpMyS3backup.php
+++ phpMyS3backup.php
@@ -2,10 +2,9 @@
 
 require_once 'config.inc.php';
 if(DEBUG_ON){error_reporting(-1);}
-$now = date("Ymd_His");
+$now = date("d-m-Y_H:i:s");
 syslog(LOG_INFO, "phpMyS3Backup - Starting run ID $now");
 require_once 'AWSSDKforPHP/sdk.class.php';
-
 
 // ********************************
 // Get list of databases
@@ -47,41 +46,29 @@ $aws = array( 	'key' => AWS_KEY,
 		);
 
 $s3 = new AmazonS3($aws);
-$bucket = strtolower('phpmys3backup-'.SRV_NAME.'-'.substr(sha1($s3->key), 0, 10) );
-$create_bucket_response = $s3->create_bucket($bucket, AmazonS3::REGION_US_E1);
-deb("creating bucket");
-if ($create_bucket_response->isOK()) {
-	$exists = $s3->if_bucket_exists($bucket);
-	while (!$exists) {
-		sleep(1);
-		$exists = $s3->if_bucket_exists($bucket);
-	}
+$bucket = BUCKET;
+foreach ($alldb as $db) {
+	$filename = "$db.sql.gz";
+	$path = "/tmp/{$now}/";
 
-	deb("bucket created - doing file add");
+	$s3->batch()->create_object($bucket, $now."/".$filename, array(
+		'fileUpload' => $path.$filename,
+		'storage' => AmazonS3::STORAGE_REDUCED,
+		'acl' => AmazonS3::ACL_PRIVATE,
+		'encryption' => 'AES256'
+	));
+}
 
-	foreach ($alldb as $db) {
-		$filename = "$db.sql.gz";
-		$path = "/tmp/{$now}/";
+deb("files setup done - uploading");
 
-		$s3->batch()->create_object($bucket, $now."/".$filename, array(
-			'fileUpload' => $path.$filename,
-			'storage' => AmazonS3::STORAGE_REDUCED,
-			'acl' => AmazonS3::ACL_PRIVATE,
-			'encryption' => 'AES256'
-		));
-	}
+$file_upload_response = $s3->batch()->send();
 
-	deb("files setup done - uploading");
+deb("upload done - waiting for ok");
 
-	$file_upload_response = $s3->batch()->send();
-
-	deb("upload done - waiting for ok");
-
-	if(DEBUG_ON){
-		if ($file_upload_response->areOK()) {
-			foreach ($alldb as $db) {
-				print $s3->get_object_url($bucket, $now."/".$db.".sql.gz", '5 minutes') . PHP_EOL . PHP_EOL;
-			}
+if(DEBUG_ON){
+	if ($file_upload_response->areOK()) {
+		foreach ($alldb as $db) {
+			print $s3->get_object_url($bucket, $now."/".$db.".sql.gz", '5 minutes') . PHP_EOL . PHP_EOL;
 		}
 	}
 }
@@ -94,12 +81,9 @@ system("rm -rf /tmp/{$now}/");
 deb("DONE");
 syslog(LOG_INFO, "phpMyS3Backup - completed run ID $now");
 
-
 // ********************************
 // Done!
 // ********************************
-
-
 
 function deb ($msg) {
 	if(DEBUG_ON) { print $msg . "\n"; }
