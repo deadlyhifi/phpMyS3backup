fd4e85b19756ab1950a2af0238290e3aa0c7d09c
Tom de Bruin <tom@deadlyhifi.com>
2012-07-10 09:27:04 +0100

    Added backup folder name definition and added instructions for Object Expiration
    Object Expiration is to set items to auto delete in  S3.

--- phpMyS3backup.php
+++ phpMyS3backup.php
@@ -8,14 +8,15 @@ define('DEBUG_ON', FALSE);										//Either: TRUE or FALSE
 define('AWS_KEY', '');											// https://aws-portal.amazon.com/gp/aws/securityCredentials
 define('AWS_SECRET', '');										// https://aws-portal.amazon.com/gp/aws/securityCredentials
 define('BUCKET', '');											// Bucket to save to.
+define('BACKUP_FOLDER', 'mysql_backups');						// Folder to save to.
 
 date_default_timezone_set('Europe/London');						// Timezone, choose yours: http://uk3.php.net/manual/en/timezones.php
 
 // ********************************
-// Let's Go!
+// Lets Go
 // ********************************
 if(DEBUG_ON){error_reporting(-1);}
-$now = date("d-m-Y_H:i:s");
+$now = date("Y-m-d_H:i:s");
 syslog(LOG_INFO, "phpMyS3Backup - Starting run ID $now");
 require_once 'AWSSDKforPHP/sdk.class.php';
 
@@ -24,12 +25,17 @@ require_once 'AWSSDKforPHP/sdk.class.php';
 // ********************************
 $alldb = array();
 $GLOBALS['con']= new PDO('mysql:host='.DB_HOST, DB_USER, DB_PASS);
-$res = $GLOBALS['con']->query("SHOW DATABASES");
-while($row = $res->fetch()) {
-	if($row['Database'] != "information_schema"){
-		$alldb[] = $row['Database'];
-		deb("Database found: {$row['Database']}");
+try {
+	$res = $GLOBALS['con']->query("SHOW DATABASES");
+	while($row = $res->fetch()) {
+		if($row['Database'] != "information_schema"){
+			$alldb[] = $row['Database'];
+			deb("Database found: {$row['Database']}");
+		}
 	}
+}
+catch (PDOException $e) {
+	print $e->getMessage();
 }
 
 // ********************************
@@ -52,20 +58,19 @@ foreach($alldb as $db){
 // ********************************
 // Upload the files to S3
 // ********************************
-$aws = array(
+$aws = array( 
 		'key' => AWS_KEY,
 		'secret' => AWS_SECRET,
 		'default_cache_config' => '',
 		'certificate_authority' => false
-		);
+	);
 
 $s3 = new AmazonS3($aws);
-$bucket = BUCKET;
 foreach ($alldb as $db) {
 	$filename = "$db.sql.gz";
 	$path = "/tmp/{$now}/";
 
-	$s3->batch()->create_object($bucket, $now."/".$filename, array(
+	$s3->batch()->create_object(BUCKET, BACKUP_FOLDER . "/" . $now . "/" . $filename, array(
 		'fileUpload' => $path.$filename,
 		'storage' => AmazonS3::STORAGE_REDUCED,
 		'acl' => AmazonS3::ACL_PRIVATE,
@@ -82,7 +87,7 @@ deb("upload done - waiting for ok");
 if(DEBUG_ON){
 	if ($file_upload_response->areOK()) {
 		foreach ($alldb as $db) {
-			print $s3->get_object_url($bucket, $now."/".$db.".sql.gz", '5 minutes') . PHP_EOL . PHP_EOL;
+			print $s3->get_object_url(BUCKET, BACKUP_FOLDER . "/" . $now . "/" . $db . ".sql.gz", '5 minutes') . PHP_EOL . PHP_EOL;
 		}
 	}
 }
@@ -90,6 +95,7 @@ if(DEBUG_ON){
 // ********************************
 // Cleanup the local filesystem
 // ********************************
+
 system("rm -rf /tmp/{$now}/");
 deb("DONE");
 syslog(LOG_INFO, "phpMyS3Backup - completed run ID $now");
@@ -97,6 +103,7 @@ syslog(LOG_INFO, "phpMyS3Backup - completed run ID $now");
 // ********************************
 // Done!
 // ********************************
+
 function deb ($msg) {
 	if(DEBUG_ON) { print $msg . "\n"; }
 }
